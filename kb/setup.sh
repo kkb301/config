fd9defd7d0a391ec7ea85331b52cb7cc5a971652
kkb301 <138193877+kkb301@users.noreply.github.com>
2023-09-29 09:36:29 +0100

    flatpak list

--- kb/setup.sh
+++ kb/setup.sh
@@ -3,3 +3,7 @@
 
 wget https://raw.githubusercontent.com/kkb301/config/main/kb/xfce4.7z
 unzip xfce4.7z
+
+flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-delete fedora
+flatpak remote-delete flathub --system
