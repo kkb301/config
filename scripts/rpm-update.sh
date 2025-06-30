9190929e8a280dc1b98f9d062f3128310f294cd7
kkb301 <138193877+kkb301@users.noreply.github.com>
2025-06-30 14:53:35 +0100

    update

--- scripts/rpm-update.sh
+++ scripts/rpm-update.sh
@@ -4,25 +4,28 @@
 
 rpm-ostree upgrade
 
-# flatpaks
 
-#flatpak upgrade -y
 
 # list of programs to install
 flatpak list --columns=application --app > /home/kevin/MEGAsync/silverblue/flatpaks.txt
 sed 's/^/flatpak install -y /' /home/kevin/MEGAsync/silverblue/flatpaks.txt > /home/kevin/MEGAsync/silverblue/flatpaks.sh
 
+#save latest firefox settings
+7z u -r  /var/home/kevin/MEGAsync/silverblue/firefox  /var/home/kevin/.mozilla/firefox
+
+#scripts
+7z u -r  /var/home/kevin/MEGAsync/silverblue/scripts /var/home/kevin/.scripts
 
 # distrobox
 
 distrobox-upgrade --all
 
-# set off a backintime save
+
 log=/home/kevin/MEGAsync/silverblue/log_file.txt
 CURRENTDATE=`date +"%D %T"`
 echo ${CURRENTDATE} : update.sh>> $log
 
-#/usr/bin/backintime backup-job
+
 
 # pause for input
 
